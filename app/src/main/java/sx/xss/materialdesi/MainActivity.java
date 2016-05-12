0685c3bb9a9a3cf64f06c377db5a2b60f00c515c
Sh1nes00n <sh1nes00n@live.com>
2016-05-12 21:34:32 +0800

    Init

---
+++ app/src/main/java/sx/xss/materialdesi/MainActivity.java
@@ -0,0 +1,13 @@
+package sx.xss.materialdesi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
